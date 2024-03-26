17de34b59f936780af186b327206545df006fce9
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2024-03-26 16:27:23 +0530

    main.mk: Wrong Java version and openjdk
    Throw warnings instead of errors

--- core/main.mk
+++ core/main.mk
@@ -171,7 +171,7 @@ $(info $(space))
 $(info Please follow the machine setup instructions at)
 $(info $(space)$(space)$(space)$(space)https://source.android.com/source/initializing.html)
 $(info ************************************************************)
-$(error stop)
+$(warning stop)
 endif
 
 # Check for the current JDK.
@@ -195,7 +195,7 @@ $(info ************************************************************)
 $(info You asked for an OpenJDK 7 build but your version is)
 $(info $(java_version_str).)
 $(info ************************************************************)
-$(error stop)
+$(warning stop)
 endif # java version is not OpenJdk
 else # if requires_openjdk
 ifneq ($(shell echo '$(java_version_str)' | grep -i openjdk),)
@@ -206,7 +206,7 @@ $(info You use OpenJDK but only Sun/Oracle JDK is supported.)
 $(info Please follow the machine setup instructions at)
 $(info $(space)$(space)$(space)$(space)https://source.android.com/source/download.html)
 $(info ************************************************************)
-$(error stop)
+$(warning stop)
 endif # java version is not Sun Oracle JDK
 endif # if requires_openjdk
 
@@ -222,7 +222,7 @@ $(info $(space))
 $(info Please follow the machine setup instructions at)
 $(info $(space)$(space)$(space)$(space)https://source.android.com/source/download.html)
 $(info ************************************************************)
-$(error stop)
+$(warning stop)
 endif
 
 
